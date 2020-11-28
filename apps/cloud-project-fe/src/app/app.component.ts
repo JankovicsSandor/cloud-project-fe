309ca47c9f70f59250f5d27a7016bb892b7a1351
JankovicsSandor <sanyika717022@gmail.com>
2020-11-28 12:42:27 +0100

    initial commit

---
+++ apps/cloud-project-fe/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cloud-project-fe-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'cloud-project-fe';
+}
